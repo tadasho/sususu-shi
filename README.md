cff4095712f853d1ff4d1733fe82ad19e4487511
ShoTada <shownfy05@gmail.com>
2017-06-09 18:15:03 +0900

    Update assign someone to issue via slack message

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # sususu-shi
+```
+$ git clone https://github.com/tadasho/sususu-shi.git
+$ cd sususu-shi/
+$ npm install
+```
+
+環境変数
+
+NODE_SLACK_ACCESS
+
+NODE_SLACK_VERIFICATION
+
+NODE_GITHUB_PASS
+
+NODE_SLACK_VERIFICATION
+
+
+on Slack
+
+@XXX review YYY#ZZZ and assign
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,15 +1,18 @@
 /*
-aws-response
+@XXX review YYY#ZZZ and assign
 
 https://api.slack.com/tutorials/events-api-using-aws-lambda
-
-awsやlambdaという文字列を発見すると返信する
 */
 
 const https: any = require('https'),
       qs: any = require('querystring'),
       VERIFICATION_TOKEN = process.env.NODE_SLACK_VERIFICATION,
-      ACCESS_TOKEN = process.env.NODE_SLACK_ACCESS;
+      ACCESS_TOKEN = process.env.NODE_SLACK_ACCESS,
+      GITHUB_PASS = process.env.NODE_GITHUB_PASS,
+      GITHUB_TEAM = process.env.NODE_GITHUB_TEAM;
+
+var GitHubApi: any = require("github");
+var github = new GitHubApi();
 
 // Verify Url - https://api.slack.com/events/url_verification
 function verify(data: any, callback: any) {
@@ -21,7 +24,7 @@ function verify(data: any, callback: any) {
 function slackProcess(event: any, callback: any) {
     // test the message for a match and not a bot
     if (!event.bot_id && /(aws|lambda)/ig.test(event.text)) {
-        var text: string = `<@${event.user}> isn't AWS Lambda awesome caffelatte?`;
+        var text: string = `<@${event.user}> isn't AWS Lambda awesome?`;
         var message: any = { 
             token: ACCESS_TOKEN,
             channel: event.channel,
@@ -35,8 +38,8 @@ function slackProcess(event: any, callback: any) {
     callback(null);
 }
 
-// Hear GitHub Review Requests
-function reviewRequests(event: any, callback: any) {
+// Hear @XXX review YYY#ZZZ and assign
+function assignToIssue(event: any, callback: any) {
     var re: any = /^\s*[@]?([^:,\s]+)[:,]?\s*review\s+(?:([^\/]+)\/)?([^#]+)#(\d+)\s*$/i;
     if (!event.bot_id && re.test(event.text)) {
         var str: string = event.text;
@@ -47,7 +50,13 @@ function reviewRequests(event: any, callback: any) {
         found[3] -> rally-app
         found[4] -> 1234
         */
-        var text: string = found[4];
+        var tadasan: any = {
+            owner: GITHUB_TEAM,
+            repo: found[3],
+            number: found[4],
+            assignees: found[1] //現段階ではslack側でgithubの@アカウント名とする必要がある。のちに設定が必要
+        }
+        var text: string = "Assigned " + found[1] + " to " + GITHUB_TEAM + "/" + found[3] + " issue #" + found[4];
         var message: any = { 
             token: ACCESS_TOKEN,
             channel: event.channel,
@@ -56,6 +65,13 @@ function reviewRequests(event: any, callback: any) {
 
         var query: string = qs.stringify(message); // prepare the querystring
         https.get(`https://slack.com/api/chat.postMessage?${query}`);
+
+        github.authenticate({
+            type: "basic",
+            username: found[1], //のちにslackとgithubの紐付けが必要
+            password: GITHUB_PASS
+        });
+        github.issues.addAssigneesToIssue(tadasan);
     }
     callback(null);
 }
@@ -67,7 +83,7 @@ function handler(data:any, context: any, callback: any) {
         case "url_verification": verify(data, callback); break;
         case "event_callback":
             slackProcess(data.event, callback);
-            reviewRequests(data.event, callback); 
+            assignToIssue(data.event, callback); 
             break;
         default: callback(null);
     }
