ea949b1a30691fec1883ac357662b735218f5848
bouzuya <m@bouzuya.net>
2017-06-24 12:45:00 +0900

    require -> import

--- src/slack-process.ts
+++ src/slack-process.ts
@@ -1,13 +1,14 @@
-const https: any = require('https'),
-      qs: any = require('querystring'),
-      ACCESS_TOKEN = process.env.NODE_SLACK_ACCESS;
+import * as https from 'https';
+import * as qs from 'querystring';
+
+const ACCESS_TOKEN = process.env.NODE_SLACK_ACCESS;
 
 // Post message to Slack - https://api.slack.com/methods/chat.postMessage
 function slackProcess(event: any, callback: any) {
     // test the message for a match and not a bot
     if (!event.bot_id && /(aws|lambda)/ig.test(event.text)) {
         var text: string = `<@${event.user}> isn't AWS Lambda awesome gorilla?` ;
-        var message: any = { 
+        var message: any = {
             token: ACCESS_TOKEN,
             channel: event.channel,
             text: text
