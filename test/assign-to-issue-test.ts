c4af776b26dedabf3d5897b48c36dd74b350eaed
bouzuya <m@bouzuya.net>
2017-06-30 23:37:09 +0900

    apply sinon

--- test/assign-to-issue-test.ts
+++ test/assign-to-issue-test.ts
@@ -1,37 +1,29 @@
 import * as assert from 'assert';
 import * as mocha from 'mocha';
 import * as proxyquire from 'proxyquire';
+import * as sinon from 'sinon';
 
 import { assignToIssue as assignToIssueT } from '../src/assign-to-issue';
 import { Config } from '../src/config';
 
 describe('assignToIssue', () => {
   let assignToIssue: typeof assignToIssueT;
+  let authenticate: sinon.SinonStub;
+  let addAssigneesToIssue: sinon.SinonStub;
+  let json: sinon.SinonStub;
+  let fetch: sinon.SinonStub;
 
   beforeEach(() => {
+    authenticate = sinon.stub();
+    addAssigneesToIssue = sinon.stub().returns(Promise.resolve());
+    json = sinon.stub().returns(Promise.resolve());
+    fetch = sinon.stub().returns(Promise.resolve({ json }));
     assignToIssue = proxyquire('../src/assign-to-issue', {
-      'github': class {
-        public authenticate(): void {
-          return;
-        }
-
-        public get issues(): { addAssigneesToIssue: any; } {
-          return {
-            addAssigneesToIssue() {
-              return Promise.resolve();
-            }
-          };
-        }
+      'github': function GitHubApi() {
+        this.authenticate = authenticate;
+        this.issues = { addAssigneesToIssue };
       },
-      'node-fetch': {
-        default() {
-          return Promise.resolve({
-            json() {
-              return Promise.resolve();
-            }
-          });
-        }
-      }
+      'node-fetch': { default: fetch }
     }).assignToIssue;
   });
 
@@ -47,6 +39,13 @@ describe('assignToIssue', () => {
         text: '@tada assign sususu-shi#1'
       }).then((value) => {
         assert(value === null);
+        assert(authenticate.callCount === 1);
+        assert(authenticate.getCall(0).args[0].type === 'basic');
+        assert(addAssigneesToIssue.callCount === 1);
+        assert(addAssigneesToIssue.getCall(0).args[0].assignees === 'tada');
+        assert(json.callCount === 1);
+        assert(fetch.callCount === 1);
+        assert((fetch.getCall(0).args[0] as string).startsWith('https:'));
       });
   });
 });
