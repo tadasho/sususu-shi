ebea1100bd3800964f8fabb32d736d5ce94140f4
ShoTada <shownfy05@gmail.com>
2017-07-03 10:08:40 +0900

    change if statement

--- src/assign-to-issue.ts
+++ src/assign-to-issue.ts
@@ -17,47 +17,45 @@ const assignToIssue = (
   }: Config,
   event: any): Promise<any> => {
   const re: any = /^\s*[@]?([^:,\s]+)[:,]?\s*assign\s+(?:([^\/]+)\/)?([^#]+)#(\d+)\s*$/i;
-  if (!event.bot_id && re.test(event.text)) {
-    const str: string = event.text;
-    const found: string[] = str.match(re);
-    /*
-    @userName assign yourRepositoryName#1234
-    found[1] -> userName
-    found[3] -> yourRepositoryName
-    found[4] -> 1234
-    */
-    const assignee: any = {
-      assignees: found[1],
-      number: found[4],
-      owner: githubTeam,
-      repo: found[3]
-    };
-    const text: string = 'Assigned ' + found[1] + ' to ' + githubTeam + '/' + found[3] + ' issue#' + found[4];
-    const message: any = {
-      channel: event.channel,
-      text,
-      token: accessToken
-    };
-
-    github.authenticate({
-      password: githubPass,
-      type: 'basic',
-      username: githubUsername // のちにslackとgithubの紐付けが必要
-    });
-    return github.issues.addAssigneesToIssue(assignee).then(() => {
-      const query: string = qs.stringify(message); // prepare the querystring
-      return fetch(`https://slack.com/api/chat.postMessage?${query}`);
-    }).then((response) => {
-      // console.log(response);
-      return response.json();
-    }).then((obj) => {
-      console.log(obj);
-      return null;
-    });
-  }
-  else {
+  if (event.bot_id || !re.test(event.text)) 
     return Promise.resolve(null);
-  }
+
+  const str: string = event.text;
+  const found: string[] = str.match(re);
+  /*
+  @userName assign yourRepositoryName#1234
+  found[1] -> userName
+  found[3] -> yourRepositoryName
+  found[4] -> 1234
+  */
+  const assignee: any = {
+    assignees: found[1],
+    number: found[4],
+    owner: githubTeam,
+    repo: found[3]
+  };
+  const text: string = 'Assigned ' + found[1] + ' to ' + githubTeam + '/' + found[3] + ' issue#' + found[4];
+  const message: any = {
+    channel: event.channel,
+    text,
+    token: accessToken
+  };
+
+  github.authenticate({
+    password: githubPass,
+    type: 'basic',
+    username: githubUsername // のちにslackとgithubの紐付けが必要
+  });
+  return github.issues.addAssigneesToIssue(assignee).then(() => {
+    const query: string = qs.stringify(message); // prepare the querystring
+    return fetch(`https://slack.com/api/chat.postMessage?${query}`);
+  }).then((response) => {
+    // console.log(response);
+    return response.json();
+  }).then((obj) => {
+    console.log(obj);
+    return null;
+  });
 };
 
 export { assignToIssue };
